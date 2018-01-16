2cef5c854f5649972f410de4429602cc77b50acd
FengShi0705 <fs2813@ic.ac.uk>
2018-01-16 18:56:44 +0000

    generate set of games ok
    including success data and unsuccessful data

--- RL.py
+++ RL.py
@@ -16,94 +16,98 @@ import utils
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from PIL import Image, ImageDraw
+from mysolution import Create_sample
 
 def build_neuralnetwork(height,width):
-    x = tf.placeholder(tf.float32,shape= [None,width*height], name='input_puzzles')
-    y_ = tf.placeholder(tf.float32, shape = [None], name='labels')
-    is_training = tf.placeholder(tf.bool, name='is_training')
+    myGraph = tf.Graph()
+    with myGraph.as_default():
+        x = tf.placeholder(tf.float32,shape= [None,width*height], name='input_puzzles')
+        y_ = tf.placeholder(tf.float32, shape = [None], name='labels')
+        is_training = tf.placeholder(tf.bool, name='is_training')
 
-    def conv_block(ipt, filter_kernalsize, channels_in, channels_out, is_training, name):
-        with tf.name_scope(name):
-            w = tf.Variable(tf.truncated_normal([filter_kernalsize[0], filter_kernalsize[1], channels_in,channels_out], stddev=0.1), name="w")
-            conv = tf.nn.conv2d(ipt,w,strides=[1,1,1,1],padding="SAME",name="conv")
-            bn = tf.layers.batch_normalization(
-                inputs=conv,
-                momentum=0.9,
-                training=is_training,
-                name=name+"_bn"
-            )
-            act = tf.nn.relu(bn,name='relu')
-            return act
+        def conv_block(ipt, filter_kernalsize, channels_in, channels_out, is_training, name):
+            with tf.name_scope(name):
+                w = tf.Variable(tf.truncated_normal([filter_kernalsize[0], filter_kernalsize[1], channels_in,channels_out], stddev=0.1), name="w")
+                conv = tf.nn.conv2d(ipt,w,strides=[1,1,1,1],padding="SAME",name="conv")
+                bn = tf.layers.batch_normalization(
+                    inputs=conv,
+                    momentum=0.9,
+                    training=is_training,
+                    name=name+"_bn"
+                )
+                act = tf.nn.relu(bn,name='relu')
+                return act
 
-    def residual_block(ipt, channels, is_training, name):
-        with tf.name_scope(name):
-            w1 = tf.Variable(tf.truncated_normal([3,3,channels,channels], stddev=0.1), name="w1")
-            conv1 = tf.nn.conv2d(ipt,w1, strides=[1,1,1,1], padding="SAME", name='conv1')
-            bn1 = tf.layers.batch_normalization(
-                inputs=conv1,
-                momentum=0.9,
-                training=is_training,
-                name=name+"_bn1"
-            )
-            rl1 = tf.nn.relu(bn1, name="rl1")
+        def residual_block(ipt, channels, is_training, name):
+            with tf.name_scope(name):
+                w1 = tf.Variable(tf.truncated_normal([3,3,channels,channels], stddev=0.1), name="w1")
+                conv1 = tf.nn.conv2d(ipt,w1, strides=[1,1,1,1], padding="SAME", name='conv1')
+                bn1 = tf.layers.batch_normalization(
+                    inputs=conv1,
+                    momentum=0.9,
+                    training=is_training,
+                    name=name+"_bn1"
+                )
+                rl1 = tf.nn.relu(bn1, name="rl1")
 
-            w2 =  tf.Variable(tf.truncated_normal([3,3,channels,channels], stddev=0.1), name="w2")
-            conv2 = tf.nn.conv2d(rl1, w2, strides=[1,1,1,1], padding="SAME", name="conv2")
-            bn2 = tf.layers.batch_normalization(
-                inputs=conv2,
-                momentum=0.9,
-                training=is_training,
-                name=name+"_bn2"
-            )
-            skip_conn = bn2 + ipt
-            rl2 = tf.nn.relu(skip_conn, name='rl2')
+                w2 =  tf.Variable(tf.truncated_normal([3,3,channels,channels], stddev=0.1), name="w2")
+                conv2 = tf.nn.conv2d(rl1, w2, strides=[1,1,1,1], padding="SAME", name="conv2")
+                bn2 = tf.layers.batch_normalization(
+                    inputs=conv2,
+                    momentum=0.9,
+                    training=is_training,
+                    name=name+"_bn2"
+                )
+                skip_conn = bn2 + ipt
+                rl2 = tf.nn.relu(skip_conn, name='rl2')
 
-            return rl2
+                return rl2
 
-    def linear_fullyconnect(ipt, in_dim, out_dim, name):
-        with tf.name_scope(name):
-            w = tf.Variable( tf.truncated_normal([in_dim, out_dim],stddev=0.1) , name="w")
-            b = tf.Variable( tf.constant(0.1, shape=[out_dim]), name='bias' )
-            h_linear = tf.matmul( ipt, w ) + b
-            return h_linear
+        def linear_fullyconnect(ipt, in_dim, out_dim, name):
+            with tf.name_scope(name):
+                w = tf.Variable( tf.truncated_normal([in_dim, out_dim],stddev=0.1) , name="w")
+                b = tf.Variable( tf.constant(0.1, shape=[out_dim]), name='bias' )
+                h_linear = tf.matmul( ipt, w ) + b
+                return h_linear
 
 
 
-    x_puzzle = tf.reshape(x,[-1, height, width, 1])
-    hidden = conv_block(x_puzzle, [3,3], 1, 256, is_training,"conv_block")
+        x_puzzle = tf.reshape(x,[-1, height, width, 1])
+        hidden = conv_block(x_puzzle, [3,3], 1, 256, is_training,"conv_block")
 
-    for i in range(1,4):
-        hidden = residual_block(hidden, 256, is_training, 'residual_block'+str(i) )
+        for i in range(1,4):
+            hidden = residual_block(hidden, 256, is_training, 'residual_block'+str(i) )
 
-    #output conv
-    hidden = conv_block(hidden, [1,1], 256, 1, is_training, 'conv_block_out')
+        #output conv
+        hidden = conv_block(hidden, [1,1], 256, 1, is_training, 'conv_block_out')
 
-    #fully connect to 256
-    hidden_flat = tf.reshape(hidden, [-1, width*height*1])
-    h_fc1 = tf.nn.relu( linear_fullyconnect(hidden_flat, width*height*1, 256, 'fully_connect_1') )
+        #fully connect to 256
+        hidden_flat = tf.reshape(hidden, [-1, width*height*1])
+        h_fc1 = tf.nn.relu( linear_fullyconnect(hidden_flat, width*height*1, 256, 'fully_connect_1') )
 
-    #fully connect to scalar
-    h_fc2 = linear_fullyconnect(h_fc1, 256, 1, 'fully_connect_2')
-    y = tf.tanh(h_fc2)
-    y = tf.reshape(y, shape=[-1,], name='predictions')
+        #fully connect to scalar
+        h_fc2 = linear_fullyconnect(h_fc1, 256, 1, 'fully_connect_2')
+        y = tf.tanh(h_fc2)
+        y = tf.reshape(y, shape=[-1,], name='predictions')
 
-    lossL2 = tf.add_n([tf.nn.l2_loss(variable) for variable in tf.trainable_variables()
-                       if 'bias' not in variable.name]) * 0.0001
+        lossL2 = tf.add_n([tf.nn.l2_loss(variable) for variable in tf.trainable_variables()
+                           if 'bias' not in variable.name]) * 0.0001
 
-    mse = tf.reduce_mean( tf.squared_difference(y,y_) )
-    loss = tf.add(mse, lossL2, name='total_loss')
+        mse = tf.reduce_mean( tf.squared_difference(y,y_) )
+        loss = tf.add(mse, lossL2, name='total_loss')
 
-    #summary
-    tf.summary.scalar('loss',loss)
-    merged = tf.summary.merge_all()
+        #summary
+        tf.summary.scalar('loss',loss)
+        merged = tf.summary.merge_all()
 
-    #train
-    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-    with tf.control_dependencies(update_ops):
-        train_step = tf.train.AdamOptimizer(1e-4).minimize(loss, name='train_mini')
+        #train
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            train_step = tf.train.AdamOptimizer(1e-4).minimize(loss, name='train_mini')
 
-    with tf.Session() as sess:
-        return sess
+    sess = tf.Session(graph=myGraph)
+    assert sess.graph is myGraph
+    return sess
 
 
 
@@ -158,7 +162,7 @@ class Node(object):
             child_state = deepcopy(self.state)
             for p in [0, 1, 2, 3]:
                 (x_, y_) = Goldenpositions[shape][p] - Goldenpositions[shape][0] + np.array([x, y])
-                if self.withinrange(x_,y_,self.row,self.col) and child_state[y_][x_] == 1:
+                if withinrange(x_,y_,self.row,self.col) and child_state[y_][x_] == 1:
                     child_state[y_][x_] = 0
                     continue
                 else:
@@ -180,11 +184,11 @@ class Node(object):
 
         return
 
-    def withinrange(self,x, y, row, col):
-        if x >= 0 and x < col and y >= 0 and y < row:
-            return True
-        else:
-            return False
+def withinrange(x, y, row, col):
+    if x >= 0 and x < col and y >= 0 and y < row:
+        return True
+    else:
+        return False
 
 
 
@@ -278,7 +282,7 @@ class Game(object):
                     else:
                         gamedata.append( (np.reshape(node.state, [-1]), self.current_realnode.V) )
 
-                return gamedata
+                return gamedata, self.current_realnode.V
             else:
                 self.play_one_move(self.current_realnode)
 
@@ -294,6 +298,71 @@ class Game(object):
         self.real_nodepath.append(self.current_realnode)
 
         return
+
+
+def play_games(N, size, prob_blank_range, eval_sess, L_backup, n_search):
+    """
+    if success, collect real move data(1.0)
+    if unsuccess, collect anwser(1.0) and real moves(-1.0) which are different for anwser
+    :param N:
+    :param size:
+    :param prob_blank_range:
+    :param eval_sess:
+    :param L_backup:
+    :param n_search:
+    :return:
+    """
+
+    Data = []
+    for i in range(0,N):
+        for prob_blank in prob_blank_range:
+            sample = Create_sample(size,size,prob_blank)
+            sample.add_pieces()
+            target,solution = sample.T,sample.S
+
+            game = Game(target,n_search, L_backup, eval_sess)
+            gamedata, result = game.play()
+            if result<0:
+                rightdata = solve_game(target,solution)
+                Data.extend(rightdata)
+                for j in range(len(gamedata)):
+                    if not np.array_equal(rightdata[j][0],gamedata[j][0]):
+                        Data.append( gamedata[j] )
+            else:
+                Data.extend(gamedata)
+
+    return Data
+
+
+def solve_game(T, S):
+    """
+    Generate successful game data solving the target
+    :param T: target
+    :param S: solution of the target
+    :return:
+    """
+    target = deepcopy(T)
+    solution = deepcopy(S)
+    row = T.shape[0]
+    col = T.shape[1]
+
+    data = []
+    data.append( ( np.reshape(target, [-1]), 1.0) )
+
+    for y in range(0, row):
+        for x in range(0, col):
+            if target[y][x] == 1:
+                target = deepcopy(target)
+                (shape,pos) = solution[y][x]
+                assert pos==0, 'top-left corner pos should be 0'
+                for p in [0, 1, 2, 3]:
+                    (x_, y_) = Goldenpositions[shape][p] - Goldenpositions[shape][0] + np.array([x, y])
+                    assert withinrange(x_,y_,row,col),'x,y not withinrange'
+                    assert target[y_][x_]==1, '(x,y) at target not to be 1'
+                    target[y_][x_] = 0
+                data.append(( np.reshape(target, [-1]) , 1.0))
+    return data
+
 
 
 def visualisation(target, solution):
@@ -339,31 +408,31 @@ def visualisation(target, solution):
     plt.show()
 
 if __name__ == "__main__":
-    sess = build_neuralnetwork(20, 20)
+    sess = build_neuralnetwork(10, 10)
     sess.run(tf.global_variables_initializer())
     r1 = 0
     r2 = 0
-    for i in range(0,100):
+    for i in range(0,1):
         print('--------------')
-        target = utils.generate_target(width=20, height=20, density=0.6)
+        target = utils.generate_target(width=20, height=20, density=0.9)
         #target=[[0, 1, 1, 0, 0, 0, 1, 1, 1, 1], [0, 0, 1, 1, 1, 1, 1, 1, 0, 1], [0, 0, 0, 0, 1, 1, 1, 0, 1, 1], [1, 1, 1, 1, 0, 1, 0, 1, 1, 1], [0, 1, 1, 1, 0, 1, 0, 1, 0, 1], [0, 1, 1, 1, 1, 1, 1, 1, 0, 0], [1, 1, 0, 0, 1, 1, 0, 1, 0, 0], [1, 0, 0, 0, 1, 1, 1, 0, 0, 1], [1, 0, 0, 1, 1, 1, 0, 1, 1, 1], [0, 1, 1, 1, 1, 0, 0, 0, 0, 0]]
-
+        print('target', np.array(target))
 
         game1 = Game(target,100,5,sess)
-        game2 = RLnode.Game(target,100,5,sess)
+        #game2 = RLnode.Game(target,100,5,sess)
         game1data = game1.play()
-        game2data = game2.play()
-        if len(game1data)>len(game2data):
-            r1 += 1
-        if len(game1data)<len(game2data):
-            r2 += 1
+        #game2data = game2.play()
+        #if len(game1data)>len(game2data):
+        #    r1 += 1
+        #if len(game1data)<len(game2data):
+        #    r2 += 1
 
-        print('backup+init edge wins: {}'.format(r1))
-        print('backup node wins: {}'.format(r2))
+        #print('backup+init edge wins: {}'.format(r1))
+        #print('backup node wins: {}'.format(r2))
 
-        """for step in game1data:
-            visualisation(target,np.reshape(step[0],[10,10]))
-        for step in game2data:
+        for step in game1data:
+            print(np.reshape(step[0],[20,20]),step[1])
+        """for step in game2data:
             visualisation(target, np.reshape(step[0], [10, 10]))"""
 
     """target = np.array([[1, 1, 1, 1, 0, 0, 0, 1, 1, 1],
