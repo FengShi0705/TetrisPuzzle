24ebaf23f8f8dbbd51c6fe9f717f5c79e2c4f22b
FengShi0705 <fs2813@ic.ac.uk>
2018-01-29 16:14:34 +0000

    try_supervised ok for google_colab

--- try_supervised.py
+++ try_supervised.py
@@ -7,9 +7,10 @@ import  numpy as np
 from data_preprocessing import Goldenpositions,data_batch_iter
 from copy import deepcopy
 import json
+import pickle
 import multiprocessing
 
-def build_neuralnetwork(height,width):
+def build_neuralnetwork_crossentropy(height,width):
     myGraph = tf.Graph()
     with myGraph.as_default():
         x = tf.placeholder(tf.float32,shape= [None,width*height], name='input_puzzles')
@@ -98,6 +99,206 @@ def build_neuralnetwork(height,width):
     assert sess.graph is myGraph
     return sess
 
+def build_neuralnetwork(height,width,nframes, n_resb_blocks, Tetris_filtering=False):
+    def Tetris_weight(nframes):
+        w = np.array([ [[0, 0, 0, 0],
+                        [0, 1, 1, 0],
+                        [0, 1, 1, 0],
+                        [0, 0, 0, 0]],
+
+                       [[1, 0, 0, 0],
+                        [1, 0, 0, 0],
+                        [1, 0, 0, 0],
+                        [1, 0, 0, 0]],
+
+                       [[1, 1, 1, 1],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[1, 0, 0, 0],
+                        [1, 0, 0, 0],
+                        [1, 1, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[0, 0, 1, 0],
+                        [1, 1, 1, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[1, 1, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[1, 1, 1, 0],
+                        [1, 0, 0, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[0, 1, 0, 0],
+                        [0, 1, 0, 0],
+                        [1, 1, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[1, 1, 1, 0],
+                        [0, 0, 1, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[1, 1, 0, 0],
+                        [1, 0, 0, 0],
+                        [1, 0, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[1, 0, 0, 0],
+                        [1, 1, 1, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[1, 0, 0, 0],
+                        [1, 1, 0, 0],
+                        [1, 0, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[0, 1, 0, 0],
+                        [1, 1, 1, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[0, 1, 0, 0],
+                        [1, 1, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[1, 1, 1, 0],
+                        [0, 1, 0, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[0, 1, 1, 0],
+                        [1, 1, 0, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[1, 0, 0, 0],
+                        [1, 1, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[1, 1, 0, 0],
+                        [0, 1, 1, 0],
+                        [0, 0, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       [[0, 1, 0, 0],
+                        [1, 1, 0, 0],
+                        [1, 0, 0, 0],
+                        [0, 0, 0, 0]],
+
+                       ],dtype=np.float32)
+
+        tetris_w = np.array([w for i in range(nframes)])
+        tetris_w = tf.constant(tetris_w, dtype=tf.float32)
+        tetris_w = tf.transpose(tetris_w, (2,3,0,1))
+        return tetris_w
+
+    myGraph = tf.Graph()
+    with myGraph.as_default():
+        x = tf.placeholder(tf.float32,shape= [None,nframes*height*width], name='input_puzzles')
+        y_ = tf.placeholder(tf.float32, shape = [None], name='labels')
+        is_training = tf.placeholder(tf.bool, name='is_training')
+        learning_rate = tf.placeholder(tf.float32, shape=(), name='learning_rate')
+
+        def conv_block(ipt, filter_kernalsize, channels_in, channels_out, is_training, name):
+            with tf.name_scope(name):
+                w = tf.Variable(tf.truncated_normal([filter_kernalsize[0], filter_kernalsize[1], channels_in,channels_out], stddev=0.1), name="w")
+                conv = tf.nn.conv2d(ipt,w,strides=[1,1,1,1],padding="SAME",name="conv")
+                bn = tf.layers.batch_normalization(
+                    inputs=conv,
+                    momentum=0.9,
+                    training=is_training,
+                    name=name+"_bn"
+                )
+                act = tf.nn.relu(bn,name='relu')
+                return act
+
+        def residual_block(ipt, channels, is_training, name):
+            with tf.name_scope(name):
+                w1 = tf.Variable(tf.truncated_normal([3,3,channels,channels], stddev=0.1), name="w1")
+                conv1 = tf.nn.conv2d(ipt,w1, strides=[1,1,1,1], padding="SAME", name='conv1')
+                bn1 = tf.layers.batch_normalization(
+                    inputs=conv1,
+                    momentum=0.9,
+                    training=is_training,
+                    name=name+"_bn1"
+                )
+                rl1 = tf.nn.relu(bn1, name="rl1")
+
+                w2 =  tf.Variable(tf.truncated_normal([3,3,channels,channels], stddev=0.1), name="w2")
+                conv2 = tf.nn.conv2d(rl1, w2, strides=[1,1,1,1], padding="SAME", name="conv2")
+                bn2 = tf.layers.batch_normalization(
+                    inputs=conv2,
+                    momentum=0.9,
+                    training=is_training,
+                    name=name+"_bn2"
+                )
+                skip_conn = bn2 + ipt
+                rl2 = tf.nn.relu(skip_conn, name='rl2')
+
+                return rl2
+
+        def linear_fullyconnect(ipt, in_dim, out_dim, name):
+            with tf.name_scope(name):
+                w = tf.Variable( tf.truncated_normal([in_dim, out_dim],stddev=0.1) , name="w")
+                b = tf.Variable( tf.constant(0.1, shape=[out_dim]), name='bias' )
+                h_linear = tf.matmul( ipt, w ) + b
+                return h_linear
+
+
+
+        x_puzzle = tf.transpose( tf.reshape(x,[-1, nframes, height, width]), (0,2,3,1) )
+
+        if Tetris_filtering: # Filtering by tetris pieces
+            tetris_w = Tetris_weight(nframes)
+            x_puzzle = tf.nn.conv2d(x_puzzle,tetris_w, strides=[1,1,1,1], padding="SAME", name='tetris_conv') # tetris filtering by conv
+            hidden = conv_block(x_puzzle, [3, 3], 19, 256, is_training, "conv_block")
+        else:
+            hidden = conv_block(x_puzzle, [3,3], nframes, 256, is_training,"conv_block")
+
+        for i in range(1,n_resb_blocks+1):
+            hidden = residual_block(hidden, 256, is_training, 'residual_block'+str(i) )
+
+        #output conv
+        hidden = conv_block(hidden, [1,1], 256, 1, is_training, 'conv_block_out')
+
+        #fully connect to 256
+        hidden_flat = tf.reshape(hidden, [-1, width*height*1])
+        h_fc1 = tf.nn.relu( linear_fullyconnect(hidden_flat, width*height*1, 256, 'fully_connect_1') )
+
+        #fully connect to scalar
+        h_fc2 = linear_fullyconnect(h_fc1, 256, 1, 'fully_connect_2')
+        y = tf.tanh(h_fc2)
+        y = tf.reshape(y, shape=[-1,], name='predictions')
+
+        lossL2 = tf.add_n([tf.nn.l2_loss(variable) for variable in tf.trainable_variables()
+                           if 'bias' not in variable.name]) * 0.0001
+
+        mse = tf.reduce_mean( tf.squared_difference(y,y_), name='MSError' )
+        loss = tf.add(mse, lossL2, name='total_loss')
+
+        #summary
+        tf.summary.scalar('loss',loss)
+        merged = tf.summary.merge_all()
+
+        #train
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss, name='train_mini')
+
+    sess = tf.Session(graph=myGraph)
+    assert sess.graph is myGraph
+    return sess
 
 class Node(object):
 
@@ -346,21 +547,33 @@ def solve_game(T, S):
 #    pool.starmap(begin_play, [('RLsamples_{}'.format(i),10000,) for i in range(8)])
 #    print('Finish Done')
 
-if __name__=='__main__':
-    data=[]
+def Main(dataset, savemodel, nframes, n_res_blocks,Tetris_filtering):
+
     #for filename in ['RL_samples_2.txt']:
-    for filename in ['RLsamples_{}_10000.txt'.format(i) for i in range(8)]:
-        with open(filename,'r') as f:
-            for line in f:
-                d = json.loads(line.strip())
-                #data.append(d)
-                data.append(( np.array(d[0],dtype=np.float32), np.array(d[1],dtype=np.float32) ))
+    #for filename in ['RLsamples_{}_10000.txt'.format(i) for i in range(1)]:
+    #    with open(filename,'r') as f:
+    #        for line in f:
+    #            d = json.loads(line.strip())
+    #            #data.append(d)
+    #            data.append(( np.array(d[0],dtype=np.float32), np.array(d[1],dtype=np.float32) ))
+
+    epoch=10
+    print('epoch:{}'.format(epoch))
+    learning_rate = 1e-4
+    print('learning_rate:{}'.format(learning_rate))
+    print('Tetris_filtering:{}',format(Tetris_filtering))
+
+    print('training data: {}'.format(dataset))
+    with open(dataset,'rb') as f:
+        #data = pickle.loads(pickle.load(f))
+        data = pickle.load(f)
+    print('number of total data: {}'.format(len(data)))
 
     data = np.array(data)
     shuffle_indices = np.random.permutation(np.arange(len(data)))
     shuffled_data = data[shuffle_indices]
     train_sample_index = int(0.8 * float(len(data)))
-    training_data, test_data = shuffled_data[:train_sample_index], shuffled_data[train_sample_index:]
+    training_data, test_data = shuffled_data[:len(data)-2000], shuffled_data[len(data)-2000:]
     print('Number of training data samples: {}'.format(len(training_data)))
     print('Number of test data samples: {}'.format(len(test_data)))
     test_x, test_y = zip(*test_data)
@@ -368,37 +581,46 @@ if __name__=='__main__':
     test_y = np.array(test_y)
 
     #train
-    sess = build_neuralnetwork(20, 20)
+    sess = build_neuralnetwork(20,20,nframes,n_res_blocks, Tetris_filtering)
     with sess:
         sess.run(tf.global_variables_initializer())
-        batches = data_batch_iter(training_data, 64, 3)
+        batches = data_batch_iter(training_data, 32, epoch)
         i = 0
         saver=tf.train.Saver()
-        best_test_acc = 0.0
+        minloss = float('inf')
         for inputdata,outputdata in batches:
             if i % 100 == 0:
-                train_accuracy = sess.run('accuracy:0', feed_dict={
+                trainloss = sess.run('MSError:0', feed_dict={
                     'input_puzzles:0': inputdata, 'labels:0': outputdata, 'is_training:0': False})
-                print('step %d, training accuracy %g' % (i, train_accuracy))
+                print('step %d, train loss %g' % (i, trainloss))
 
             if i % 1000 == 0:
-                start_test_id = np.random.randint(len(test_data)-5000)
-                test_acc = sess.run('accuracy:0', feed_dict={
-                    'input_puzzles:0': test_x[start_test_id:start_test_id+5000], 'labels:0': test_y[start_test_id:start_test_id+5000], 'is_training:0': False})
-                print('step %d, test accuracy %g' % (i, test_acc))
-                if test_acc > best_test_acc:
-                    best_test_acc = test_acc
-                    save_path = saver.save(sess, '../TetrisPuzzle_RL_data/saver/supervised.ckpt')
-                    print("best Test acc. Model saved in file: %s" % save_path)
+                test_loss = sess.run('MSError:0', feed_dict={
+                    'input_puzzles:0': test_x, 'labels:0': test_y, 'is_training:0': False})
+                print('step %d, test loss %g' % (i, test_loss))
+                if test_loss < minloss:
+                    minloss = test_loss
+                    save_path = saver.save(sess, './{}.ckpt'.format(savemodel))
+                    print("min Test lose. Model saved in file: %s" % save_path)
 
 
             sess.run('train_mini',
-                     feed_dict={'input_puzzles:0': inputdata, 'labels:0': outputdata, 'is_training:0': True})
+                     feed_dict={'input_puzzles:0': inputdata, 'labels:0': outputdata, 'is_training:0': True, 'learning_rate:0':learning_rate })
             i += 1
 
 
-        print('final test accuracy %g' % sess.run('accuracy:0', feed_dict={
-            'input_puzzles:0': test_x[0:6000], 'labels:0': test_y[0:6000], 'is_training:0': False}))
+        final_testloss = sess.run('MSError:0', feed_dict={
+                    'input_puzzles:0': test_x, 'labels:0': test_y, 'is_training:0': False})
+        print('final test loss {}'.format(final_testloss) )
+        if final_testloss < minloss:
+            minloss = final_testloss
+            save_path = saver.save(sess, './{}.ckpt'.format(savemodel))
+            print("min Test lose. Model saved in file: %s" % save_path)
 
+        print('During this training, minloss is {}'.format(minloss))
+
+
+if __name__=='__main__':
+    Main(None,None,None,None,None)
 
 
