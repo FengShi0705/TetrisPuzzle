ab0598ddc19b4338c4dbaff42a6e85aee03dbef4
FengShi0705 <shi.feng.nwpu@gmail.com>
2018-01-31 17:34:18 +0000

    TD search

--- RL_TDsearch.py
+++ RL_TDsearch.py
@@ -1,4 +1,198 @@
 # this script use TD search:
-# 1. use golden data to assist the search:
-# (1). Use e-greedy instead of UCB, otherwise it will always go through the golden data
-# (2). three steps: Select, Store and Train
+# 1. Do not use golden right data
+# 2. reward configuration: (1) final -1 ,1, (2) immediate reword 4/total_blocks + (total_blocks-4)/total_blocks * Vt+1
+# 3. value function representation (1) table lookup (2) approximator
+# 4. select action (1) e-greedy (2). UCB
+
+
+import numpy as np
+import tensorflow as tf
+from data_preprocessing import Goldenpositions,data_batch_iter
+from copy import deepcopy
+
+
+class Node(object):
+
+    def __init__(self,state,table,cumR,uniR):
+        self.table = table
+        self.state = state
+        self.cumR = cumR
+        self.uniR = uniR
+        self.expanded = False
+        self.row = self.state.shape[0]
+        self.col = self.state.shape[1]
+
+        self.N = 0
+        self.Q = 0.0
+        self.W = 0.0
+
+
+
+    def check_explore(self):
+        """
+        self.terminal indicates whether this state is terminal
+        self.V is real value: -1 for unsuccessful terminal and 1 for successful terminal
+        self.score facilitate the MCTS by considering the depth of the terminal
+        """
+        self.expanded = True
+        for y in range(0,self.row):
+            for x in range(0,self.col):
+                if self.state[y][x]==1:
+                    self.fetch_children(x,y)
+                    if len(self.children)>0:
+                        self.terminal = False
+                        return
+                    else:
+                        self.terminal = True
+                        self.score = -1.0 + self.cumR
+                        self.V = -1.0
+                        return
+
+        self.terminal = True
+        self.score = 1.0
+        self.V = 1.0
+        return
+
+    def fetch_children(self,x,y):
+        self.children = []
+        for shape in range(1,20):
+            available = True
+            child_state = deepcopy(self.state)
+            for p in [0, 1, 2, 3]:
+                (x_, y_) = Goldenpositions[shape][p] - Goldenpositions[shape][0] + np.array([x, y])
+                if withinrange(x_,y_,self.row,self.col) and child_state[y_][x_] == 1:
+                    child_state[y_][x_] = 0
+                    continue
+                else:
+                    available = False
+                    break
+
+            if available:
+                stateid = child_state.tostring()
+                if stateid in self.table:
+                    #append child
+                    self.children.append( self.table[stateid] )
+                else:
+                    # create child
+                    c_node = Node(child_state,self.table, self.cumR+self.uniR, self.uniR)
+                    # add in table
+                    self.table[stateid] = c_node
+                    #append child
+                    self.children.append(c_node)
+
+        return
+
+def withinrange(x, y, row, col):
+    if x >= 0 and x < col and y >= 0 and y < row:
+        return True
+    else:
+        return False
+
+
+
+
+
+class Simulation(object):
+    """
+    We generate training samples by using real move state rather than simulation move state,
+    because one simulation state may have multiple results to be either 1 or -1.
+     This would not be efficient in supervised learning
+    """
+
+    def __init__(self,rootnode,L,sess):
+        self.currentnode = rootnode
+        self.sess = sess
+        self.path = [rootnode]
+        self.L = L
+        self.t = 0
+
+
+    def run(self):
+
+        while True:
+            if not self.currentnode.expanded:
+                self.currentnode.check_explore()
+            if self.currentnode.terminal:
+                self.backup(self.currentnode.V)
+                break
+
+            if self.t > 0 and (self.t%self.L==0):
+                predict_value = self.sess.run('predictions:0',
+                                              feed_dict={'input_puzzles:0':np.reshape(self.currentnode.state,[1,-1]).astype(np.float32),
+                                                         'is_training:0': False}
+                                              )
+                self.backup(predict_value[0])
+                #self.backup(0.0)
+                #for child in self.currentnode.children:
+                #    child.N = 0
+                #    child.Q = 0.0
+                #    child.W = 0.0
+
+            self.currentnode = self.selectfrom(self.currentnode)
+            self.path.append(self.currentnode)
+            self.t += 1
+
+        return
+
+    def backup(self,v):
+        for node in self.path:
+            node.W += v
+            node.N += 1
+            node.Q = node.W/node.N
+        return
+
+
+    def selectfrom(self,node):
+        sum_N = np.sum([ child.N for child in node.children ])
+        value_max = (-100.0, None)
+        for child in node.children:
+            v = child.Q + ( ( np.sqrt(2*sum_N) ) / (1 + child.N) )
+            if v > value_max[0]:
+                value_max = (v, child)
+
+        return value_max[1]
+
+
+class Game(object):
+
+    def __init__(self, target, n_search, L, sess):
+        self.target = np.array(target)
+        self.table = {}
+        self.n_search = n_search
+        self.L = L
+        self.sess = sess
+
+        self.current_realnode = Node(deepcopy(self.target),self.table, 0.0, 4/np.sum(self.target))
+        self.table[self.current_realnode.state.tostring()] = self.current_realnode
+        self.real_nodepath = [self.current_realnode]
+
+    def play(self):
+
+        while True:
+            if not self.current_realnode.expanded:
+                self.current_realnode.check_explore()
+
+            if self.current_realnode.terminal:
+                gamedata = []
+                for i, node in enumerate(self.real_nodepath):
+                    if i==0:
+                        gamedata.append( (np.reshape(node.state, [-1]), 1.0) )
+                    else:
+                        gamedata.append( (np.reshape(node.state, [-1]), self.current_realnode.V) )
+
+                return gamedata, self.current_realnode.V, self.current_realnode.score
+            else:
+                self.play_one_move(self.current_realnode)
+
+
+    def play_one_move(self,startnode):
+        for i in range(0,self.n_search):
+            simulation = Simulation(startnode,self.L,self.sess)
+            simulation.run()
+
+
+        (maxQ,maxchild) = max([(child.Q, child) for child in startnode.children],key=lambda s:s[0])
+        self.current_realnode = maxchild
+        self.real_nodepath.append(self.current_realnode)
+
+        return
